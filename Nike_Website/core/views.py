7e67c99a63b2b6eff76695df21c5c1987627c923
Aashaypariskar <aashaypariskar1605@gmail.com>
2024-11-08 15:15:26 +0530

    new project

--- Nike_Website/core/views.py
+++ Nike_Website/core/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render,redirect
 
 def index(request):
     return render(request,'core/index.html')
-
-def product(request,id):
-    return render(request,'core/product.html', {'id':id})
