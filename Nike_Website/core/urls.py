f4f71d245c1a82bba920415d90536436a12cd08b
Aashaypariskar <aashaypariskar1605@gmail.com>
2024-11-09 12:26:28 +0530

    forms updated

--- Nike_Website/core/urls.py
+++ Nike_Website/core/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
+    path('registration/',views.registration,name='registration'),
+    path('login/',views.log_in,name='login'),
+    path('profile/',views.profile,name='profile'),
+    path('logout/',views.log_out, name="logout"),
+    path('changepassword/',views.changepassword, name="changepassword"),
        
 ]
