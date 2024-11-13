d20e9492ba6b3f6f9c6243cebd47a31016660a32
Aashaypariskar <aashaypariskar1605@gmail.com>
2024-11-13 16:50:43 +0530

    update cards

--- Nike_Website/core/urls.py
+++ Nike_Website/core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('',views.index,name='index'),
     path('registration/',views.registration,name='registration'),
@@ -14,3 +17,7 @@ urlpatterns = [
     path('changepassword/',views.changepassword, name="changepassword")
        
 ]
+
+#--------- THis is will add file to media folder -----------
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
