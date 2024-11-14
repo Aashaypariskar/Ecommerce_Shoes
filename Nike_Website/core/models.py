63d23324cb04b01a5c75f3a4ba70e0ffc141fffb
Aashaypariskar <aashaypariskar1605@gmail.com>
2024-11-14 16:59:46 +0530

    update

--- Nike_Website/core/models.py
+++ Nike_Website/core/models.py
@@ -15,7 +15,7 @@ class Shoes(models.Model):
     description=models.TextField()
     original_price = models.IntegerField()
     discounted_price = models.IntegerField()
-    pets_image =models.ImageField(upload_to='pet_images')  # As we are using image field we have to intall 'pillow'. And we have to Define MEDIA_URL in settings.py file so that all folder should save in media directory
+    pets_image = models.ImageField(upload_to='pet_images')  # As we are using image field we have to intall 'pillow'. And we have to Define MEDIA_URL in settings.py file so that all folder should save in media directory
 
     def __str__(self):
         return str(self.name)
